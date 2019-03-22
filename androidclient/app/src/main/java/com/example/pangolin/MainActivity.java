42b72a0ffc2116ea4738ff50678e8ecb25c2a9de
xitongsys <xitongsys@gmail.com>
2019-03-22 18:50:58 +0800

    add preferenece

--- androidclient/app/src/main/java/com/example/pangolin/MainActivity.java
+++ androidclient/app/src/main/java/com/example/pangolin/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.pangolin;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.VpnService;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -21,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
     private EditText editServer, editServerPort, editLocal, editDNS;
     private TextView viewInfo;
     private Thread sendThread;
+    SharedPreferences preferences;
+    SharedPreferences.Editor preEditor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,8 +39,15 @@ public class MainActivity extends AppCompatActivity {
         viewInfo = findViewById(R.id.infoTextView);
         editDNS = findViewById(R.id.dnsEdit);
 
-
         sendThread = null;
+
+        preferences = getPreferences(Activity.MODE_PRIVATE);
+        preEditor = preferences.edit();
+
+        editServer.setText(preferences.getString("serverIP", "192.168.0.1"));
+        editServerPort.setText(preferences.getString("serverPort", "12345"));
+        editLocal.setText(preferences.getString("localIP", "10.0.0.3/8"));
+        editDNS.setText(preferences.getString("dns", "8.8.8.8"));
 
         btDisconn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -59,8 +70,6 @@ public class MainActivity extends AppCompatActivity {
                 }else{
                     onActivityResult(0, RESULT_OK, null);
                 }
-
-
             }
         });
 
@@ -71,12 +80,24 @@ public class MainActivity extends AppCompatActivity {
         if (result == RESULT_OK) {
             Intent intent = new Intent(this, PangolinVpnService.class);
 
+            String serverIP = editServer.getText().toString();
+            String serverPort = editServerPort.getText().toString();
+            String localIp = editLocal.getText().toString();
+            String dns = editDNS.getText().toString();
+
             intent.setAction("connect");
-            intent.putExtra("serverIP", editServer.getText().toString());
-            intent.putExtra("serverPort", Integer.parseInt(editServerPort.getText().toString()));
-            intent.putExtra("localIP", editLocal.getText().toString());
-            intent.putExtra("dns", editDNS.getText().toString());
+            intent.putExtra("serverIP", serverIP);
+            intent.putExtra("serverPort", Integer.parseInt(serverPort));
+            intent.putExtra("localIP", localIp);
+            intent.putExtra("dns", dns);
             startService(intent);
+
+            preEditor.putString("serverIP", serverIP);
+            preEditor.putString("serverPort", serverPort);
+            preEditor.putString("localIP", localIp);
+            preEditor.putString("dns", dns);
+            preEditor.commit();
+
         }
     }
 
