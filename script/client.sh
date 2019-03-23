65dcd9250695e2b14bae53f34149f25128c3d081
xitongsys <xitongsys@gmail.com>
2019-03-23 14:40:58 +0800

    add readme

--- script/client.sh
+++ script/client.sh
@@ -1,7 +1,10 @@
+SERVER=""
+GW=""
+
 ip tuntap add dev tun0 mod tun
 ip addr add 10.0.0.2/24 dev tun0
 ip link set tun0 up
 
-route add 139.180.132.42 gw 192.168.43.1
+route add $SERVER gw $GW
 route add default gw 10.0.0.1
 
--- script/server.sh
+++ script/server.sh
@@ -1,7 +1,10 @@
+SERVER=""
+DEVICE=""
+
 ip tuntap add dev tun0 mod tun
 ip addr add 10.0.0.2/24 dev tun0
 ip link set tun0 up
 
 
 cat 1 >> /proc/sys/net/ipv4/ip_forward
-iptables -t nat -A POSTROUTING -o ens3 -j SNAT --to-source 139.180.132.42
+iptables -t nat -A POSTROUTING -o $DEVICE -j SNAT --to-source $SERVER
