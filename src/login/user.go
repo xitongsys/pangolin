71aafc34fcd7a00cf6c9d34d1dd5bdb1cba441d0
xitongsys <xitongsys@gmail.com>
2019-06-27 09:33:00 -0400

    ..

--- src/login/user.go
+++ src/login/user.go
@@ -2,6 +2,9 @@ package login
 
 import (
 	"encrypt"
+	"net"
+
+	"util"
 )
 
 var CHANBUFFERSIZE = 1024
@@ -12,6 +15,7 @@ type User struct {
 	Token string
 	Key string
 	Chan chan string
+	Conn net.Conn
 }
 
 func NewUser(client string, tun string, token string) *User {
@@ -23,4 +27,51 @@ func NewUser(client string, tun string, token string) *User {
 		Key: key,
 		Chan: make(chan string, CHANBUFFERSIZE),
 	}
+}
+
+func (user *User) Start() {
+	//read from client, write to channel
+	go func() {
+		for {
+			var err error
+			data, err := util.ReadPacket(user.Conn)
+			if err != nil {
+				ts.TunServer.CloseClient(clientAddr)
+				return
+			}
+
+			if ln := len(data); ln > 0 {
+				if data, err = comp.UncompressGzip(data); err == nil && len(data)>0{
+					if protocol, src, dst, err := header.GetBase(data); err == nil {
+						ts.TunServer.WriteToChannel("tcp", clientAddr, data)
+						fmt.Printf("[TcpServer][readFromClient] client:%v, protocol:%v, len:%v, src:%v, dst:%v\n", clientAddr, protocol, ln, src, dst)
+					}
+				}
+			}
+		}
+	}()
+
+	//read from channel, write to client
+	go func() {
+		for {
+			data, err := ts.TunServer.ReadFromChannel(clientAddr)
+			if err != nil {
+				return
+			}
+			if ln := len(data); ln > 0 {
+				if protocol, src, dst, err := header.GetBase(data); err == nil {
+					if _, err := util.WritePacket(conn, comp.CompressGzip(data)); err != nil {
+						ts.TunServer.CloseClient(clientAddr)
+						return
+					}
+					fmt.Printf("[TcpServer][writeToClient] client:%v, protocol:%v, len:%v, src:%v, dst:%v\n", clientAddr, protocol, ln, src, dst)
+				}
+			}
+		}
+	}()
+}
+
+func (user *User) Close() {
+	close(user.Chan)
+	user.Conn.Close()
 }
