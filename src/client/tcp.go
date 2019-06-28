17b1f912297f22ac7ca9ada2593d416ca80f6466
xitongsys <xitongsys@gmail.com>
2019-06-28 01:17:49 -0400

    ..

--- src/client/tcp.go
+++ src/client/tcp.go
@@ -51,10 +51,11 @@ func (tc *TcpClient) writeToServer() {
 		if n, err := tc.TunConn.Read(data); err == nil && n > 0 {
 			data = data[:n]
 			if protocol, src, dst, err := header.GetBase(data); err == nil {
-				data = encrypt.EncryptAES(data, encryptKey)
-				cmpData := comp.CompressGzip(data)
-				util.WritePacket(tc.TcpConn, cmpData)
-				fmt.Printf("[TcpClient][writeToServer] protocol:%v, len:%v, src:%v, dst:%v\n", protocol, n, src, dst)
+				if data, err = encrypt.EncryptAES(data, encryptKey); err == nil {
+					cmpData := comp.CompressGzip(data)
+					util.WritePacket(tc.TcpConn, cmpData)
+					fmt.Printf("[TcpClient][writeToServer] protocol:%v, len:%v, src:%v, dst:%v\n", protocol, n, src, dst)
+				}
 			}
 		}
 	}
@@ -65,10 +66,11 @@ func (tc *TcpClient) readFromServer() error {
 	for {
 		if data, err := util.ReadPacket(tc.TcpConn); err == nil {
 			if data, err := comp.UncompressGzip(data); err == nil && len(data) > 0 {
-				data = encrypt.DecryptAES(data, encryptKey)
-				if protocol, src, dst, err := header.GetBase(data); err == nil {
-					tc.TunConn.Write(data)
-					fmt.Printf("[TcpClient][readFromServer] protocol:%v, len:%v, src:%v, dst:%v\n", protocol, len(data), src, dst)
+				if data, err = encrypt.DecryptAES(data, encryptKey); err == nil {
+					if protocol, src, dst, err := header.GetBase(data); err == nil {
+						tc.TunConn.Write(data)
+						fmt.Printf("[TcpClient][readFromServer] protocol:%v, len:%v, src:%v, dst:%v\n", protocol, len(data), src, dst)
+					}
 				}
 			}
 		}
