6e0868b15d7bb2c26f274e6400613a2851489d76
Xitong Zhang <xitongsys@gmail.com>
2019-07-05 19:25:29 +0800

    ..

--- docker/pangolin_win.ps1
+++ docker/pangolin_win.ps1
@@ -7,7 +7,32 @@ $env:DOCKERNAME="pangolin"
 # $TOKENS='["token01", "token02"]'
 # $ROLE="CLIENT"
 
-. .\functions.ps1
+function installPangolin () {
+    $Adapter=(Get-NetRoute | Where-Object -FilterScript {$_.NextHop -Ne "::"} | Where-Object -FilterScript { $_.NextHop -Ne "0.0.0.0" } | Where-Object -FilterScript { ($_.NextHop.SubString(0,6) -Ne "fe80::") } | Get-NetAdapter ).Name.ToString()
+    New-VMSwitch -Name $env:SWITCHNAME -NetAdapterName $Adapter -AllowManagementOS $true
+    docker-machine create -d hyperv --hyperv-virtual-switch $env:SWITCHNAME $env:VMNAME
+    docker-machine env $env:VMNAME
+    & docker-machine.exe env $env:VMNAME | Invoke-Expression
+    docker build -t $env:DOCKERNAME .
+}
+
+function uninstallPangolin () {
+    docker-machine stop $env:VMNAME
+    docker-machine rm -f $env:VMNAME
+    Remove-VMSwitch -Name $env:SWITCHNAME -Force
+}
+
+function startPangolin () {
+    & docker-machine.exe env $env:VMNAME | Invoke-Expression
+    docker run --cap-add NET_ADMIN --cap-add NET_RAW --device /dev/net/tun:/dev/net/tun --net host --env ROLE=$env:ROLE --env SERVERIP=$env:SERVERIP --env SERVERPORT=$env:SERVERPORT --env TOKENS=$env:TOKENS pangolin
+    sleep 3
+}
+
+function stopPangolin () {
+    docker-machine stop $env:VMNAME
+    sleep 3
+}
+
 
 #UI##################################################################
 Add-Type -assembly System.Windows.Forms
@@ -95,6 +120,11 @@ function initEnv () {
     $env:SERVERPORT = $textPort.Text
     $env:TOKENS = $textTokens.Text
     $env:ROLE = $comboRole.SelectedText
+
+    $env:installPangolin=installPangolin
+    $env:uninstallPangolin=uninstallPangolin
+    $env:startPangolin=startPangolin
+    $env:stopPangolin=stopPangolin
 }
 
 function outputToGUI {
@@ -131,8 +161,7 @@ $buttonStart.Add_Click(
     {
         initEnv
         Start-Process powershell {
-            . .\functions.ps1
-            startPangolin
+            $env:startPangolin
         }
     }
 )
@@ -141,8 +170,7 @@ $buttonStop.Add_Click(
     {
         initEnv
         Start-Process powershell {
-            . .\functions.ps1
-            stopPangolin
+            $env:stopPangolin
         }
     }
 )
