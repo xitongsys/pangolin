403c3f9575c1aa6ff82e49945cc28de628d7d989
xitongsys <xitongsys@gmail.com>
2019-07-08 15:36:10 +0800

    ..

--- docker/pangolin_win.ps1
+++ docker/pangolin_win.ps1
@@ -24,6 +24,11 @@ function uninstallPangolin () {
 
 function startPangolin () {
     & docker-machine.exe env $env:VMNAME | Invoke-Expression
+    $pangolinIp=docker-machine.exe ip $env:VMNAME
+    Get-NetRoute | where { $_.DestinationPrefix -eq '0.0.0.0/0' } | select { $_.NextHop } | route delete 0.0.0.0
+    route add 0.0.0.0 mask 0.0.0.0 $pangolinIp
+    $Adapter=((Get-NetRoute | Where-Object -FilterScript {$_.NextHop -Ne "::"} | Where-Object -FilterScript { $_.NextHop -Ne "0.0.0.0" } | Where-Object -FilterScript { ($_.NextHop.SubString(0,6) -Ne "fe80::") } | Get-NetAdapter ).Name.ToString())
+    #Set-DnsClientServerAddress -InterfaceAlias $Adapter -ServerAddresses("8.8.8.8")
     docker run --cap-add NET_ADMIN --cap-add NET_RAW --device /dev/net/tun:/dev/net/tun --net host --env ROLE=$env:ROLE --env SERVERIP=$env:SERVERIP --env SERVERPORT=$env:SERVERPORT --env TOKENS=$env:TOKENS pangolin
     sleep 3
 }
@@ -163,16 +168,7 @@ $buttonUninstall.Add_Click(
 $buttonStart.Add_Click(
     {
         initEnv
-        Start-Process powershell {
-            & docker-machine.exe env $env:VMNAME | Invoke-Expression
-            $pangoinIp = docker-machine.exe ip $env:VMNAME
-            Get-NetRoute | where { $_.DestinationPrefix -eq '0.0.0.0/0' } | select { $_.NextHop } | route delete 0.0.0.0
-            route add 0.0.0.0 mask 0.0.0.0 $pangoinIp
-            $Adapter=(Get-NetRoute | Where-Object -FilterScript {$_.NextHop -Ne "::"} | Where-Object -FilterScript { $_.NextHop -Ne "0.0.0.0" } | Where-Object -FilterScript { ($_.NextHop.SubString(0,6) -Ne "fe80::") } | Get-NetAdapter ).Name.ToString()
-            Set-DnsClientServerAddress -InterfaceAlias $Adapter -ServerAddresses("8.8.8.8")
-            docker run --cap-add NET_ADMIN --cap-add NET_RAW --device /dev/net/tun:/dev/net/tun --net host --env ROLE=$env:ROLE --env SERVERIP=$env:SERVERIP --env SERVERPORT=$env:SERVERPORT --env TOKENS=$env:TOKENS pangolin
-            sleep 3
-        } 
+        Start-Process powershell -ArgumentList (startPangolin)
     }
 )
 
