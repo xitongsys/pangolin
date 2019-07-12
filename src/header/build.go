f8543322c064909ae1743e89572c1dafbc051ce7
xitongsys <xitongsys@gmail.com>
2019-07-11 21:20:45 -0400

    add build

--- src/header/build.go
+++ src/header/build.go
@@ -1,5 +1,39 @@
 package header
 
+func BuildUdpPacket(src string, dst string, data []byte) []byte {
+	srcIp, srcPort := ParseAddr(src)
+	dstIp, dstPort := ParseAddr(dst)
+
+	ipv4Header := IPv4{
+		VerIHL: 0x45,
+		Tos: 0,
+		Len: uint16(20 + 8 + len(data)),
+		Id: 0,
+		Offset: 0,
+		TTL: 255,
+		Protocol: 0x11,
+		Checksum: 0,
+		Src: Str2IP(srcIp),
+		Dst: Str2IP(dstIp),
+	}
+	ipv4Header.ResetChecksum()
+
+	udpHeader := UDP{
+		SrcPort: uint16(srcPort),
+		DstPort: uint16(dstPort),
+		Len: uint16(8 + len(data)),
+		Checksum: 0,
+	}
+
+	bs := []byte{}
+	bs = append(bs, ipv4Header.Marshal()...)
+	bs = append(bs, udpHeader.Marshal()...)
+	bs = append(bs, data...)
+	ReCalUdpCheckSum(bs)
+
+	return bs
+} 
+
 func BuildTcpPacket(src string, dst string, data []byte) []byte {
 	srcIp, srcPort := ParseAddr(src)
 	dstIp, dstPort := ParseAddr(dst)
