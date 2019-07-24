ff960eae174b5f2938741891177ef83a929dd1c6
xitongsys <xitongsys@gmail.com>
2019-07-24 13:05:27 +0800

    debug

--- util/io.go
+++ util/io.go
@@ -28,6 +28,7 @@ func WriteUntil(conn net.Conn, bufSize int, data []byte, timeout time.Duration, 
 				return
 			case <-after:
 				err = fmt.Errorf("timeout")
+				conn.Close()
 				return
 			default:
 				return
